5641f3d51eae7e0424d262f6a42d0a6d231eece6
Qianying Ruan <124208512+1239874569898@users.noreply.github.com>
2023-10-22 22:11:52 -0700

    Add files via upload

---
+++ item-list.js
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import items from "./item";
+import itemInfo from "./item.json";
+
---
+++ page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+      <main>
+        <h1 className="text-4xl font-bold m-6 text-center text-white">
+        Shopping List
+        </h1>
+        <Itemlist />
+      </main>
+    );
+  }
